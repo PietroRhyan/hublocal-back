0f9f988e9e58f0dd367d46e6dbf71fa349e9c2fa
Pietro Rhyan <pietroqjg@gmail.com>
2023-02-02 10:06:11 -0300

    Cant connect to Database

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -3,10 +3,10 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
-  port: 6500,
+  port: 5432,
+  database: "hublocal_db",
   username: "postgres",
-  password: "hublocal",
-  database: "HublocalDB",
+  password: "hublocal123",
   entities: ["./src/modules/**/entities/*.ts"],
   migrations: ["./src/infra/migrations/*.ts"],
 });
